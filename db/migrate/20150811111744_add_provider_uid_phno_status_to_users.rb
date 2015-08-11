af877b5edd10734302ff932bfab2e68e861a583e
saieswar <sai.eswar786@gmail.com>
2015-08-11 20:23:00 +0530

    Saieswar - Cleared migration error

--- db/migrate/20150811111744_add_provider_uid_phno_status_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddProviderUidPhnoStatusToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_column :users, :phone_number, :int
-    add_column :users, :status, :boolean, :default => true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811130106) do
+ActiveRecord::Schema.define(version: 20150811144343) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20150811130106) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "histories", force: :cascade do |t|
+    t.float    "quantity",   limit: 24
+    t.float    "price",      limit: 24
+    t.integer  "order_id",   limit: 4
+    t.integer  "product_id", limit: 4
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  add_index "histories", ["order_id"], name: "index_histories_on_order_id", using: :btree
+  add_index "histories", ["product_id"], name: "index_histories_on_product_id", using: :btree
 
   create_table "images", force: :cascade do |t|
     t.string   "image_src",  limit: 255
@@ -41,6 +53,16 @@ ActiveRecord::Schema.define(version: 20150811130106) do
     t.datetime "updated_at",           null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "status_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "orders", ["status_id"], name: "index_orders_on_status_id", using: :btree
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
+
   create_table "products", force: :cascade do |t|
     t.string   "product_name", limit: 255
     t.float    "price",        limit: 24
@@ -54,6 +76,12 @@ ActiveRecord::Schema.define(version: 20150811130106) do
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
   add_index "products", ["unit_id"], name: "index_products_on_unit_id", using: :btree
 
+  create_table "statuses", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "units", force: :cascade do |t|
     t.string   "unit_type",  limit: 255
     t.datetime "created_at",             null: false
@@ -61,29 +89,45 @@ ActiveRecord::Schema.define(version: 20150811130106) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",   null: false
-    t.string   "encrypted_password",     limit: 255, default: "",   null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,    null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "user_name",              limit: 255
     t.string   "provider",               limit: 255
     t.string   "uid",                    limit: 255
-    t.integer  "phone_number",           limit: 4
-    t.boolean  "status",                 limit: 1,   default: true
-    t.string   "user_name",              limit: 255
+    t.string   "phone_number",           limit: 255
+    t.boolean  "status",                 limit: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "wishlists", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "product_id", limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "wishlists", ["product_id"], name: "index_wishlists_on_product_id", using: :btree
+  add_index "wishlists", ["user_id"], name: "index_wishlists_on_user_id", using: :btree
+
+  add_foreign_key "histories", "orders"
+  add_foreign_key "histories", "products"
   add_foreign_key "images", "products"
+  add_foreign_key "orders", "statuses"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "units"
+  add_foreign_key "wishlists", "products"
+  add_foreign_key "wishlists", "users"
 end
