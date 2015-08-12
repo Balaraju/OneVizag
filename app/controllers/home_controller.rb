6e5ea0f588e091a9118c2b0ef4788a18d5987d48
santosh <korada.santoshkumar611@gmail.com>
2015-08-12 19:09:02 +0530

    santosh home controller and routes and schema

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,15 @@
 class HomeController < ApplicationController
-<<<<<<< HEAD
+
 #<<<<<<< HEAD
   def view
   	@categories=Category.all
+  end
 
-end
-  def index
-#>>>>>>> 4d2f2337bd60a51f6c6a9aea8544766e9c606105
-=======
+
   def index
   	@categories=Category.all
->>>>>>> b9293fa3481bfcb00715648a6bf4a1d62f730314
   end
-  def view
-  end
-  def products_view
+ def products_view
   	@categories=Category.all
     
   	respond_to do |format|
