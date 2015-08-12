1e11940a8effd54fdd23a119f7a90b0c8d334be3
santosh <korada.santoshkumar611@gmail.com>
2015-08-12 20:18:46 +0530

    santosh - cleared errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,21 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-root to: "home#index"
-=======
->>>>>>> c2007a75c6c6dd937f84a36892f5bacd8b8453ec
 
+root to: "home#index"
   #get 'home/index'
 
 #  root 'home#view'
 
 
-<<<<<<< HEAD
+
   
 
 devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  
-
-=======
->>>>>>> c2007a75c6c6dd937f84a36892f5bacd8b8453ec
-
-  resources :products
+resources :products
 
   
   get 'home/index'
-<<<<<<< HEAD
+
   get 'home/products_view' => 'home#products_view', as: :home_products_view
  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -31,17 +23,13 @@ devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-=======
-  get 'home/products_view'
-devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
  
-  root to: "home#index"
->>>>>>> c2007a75c6c6dd937f84a36892f5bacd8b8453ec
+  
 
   #   get 'products/:id' => 'catalog#view'
 
